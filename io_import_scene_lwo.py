e18fa018b0fb16384460a60e9cd3b4ff9c2b2ac7
campbellbarton <campbellbarton@96b784f8-4ae3-dd11-aff7-0030488c597c>
2010-09-03 16:07:10 +0000

    fix for change in blender
    git-svn-id: https://svn.blender.org/svnroot/bf-extensions/contrib/py/scripts/addons@979 96b784f8-4ae3-dd11-aff7-0030488c597c

--- io_import_scene_lwo.py
+++ io_import_scene_lwo.py
@@ -1111,7 +1111,7 @@ class IMPORT_OT_lwo(bpy.types.Operator):
         return {'FINISHED'}
     
     def invoke(self, context, event):
-        wm= context.manager
+        wm= context.window_manager
         wm.add_fileselect(self)
         return {'RUNNING_MODAL'}
 
